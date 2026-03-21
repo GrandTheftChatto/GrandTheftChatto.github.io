a42ba8091c16af064f74e1f77b3a6377f2cd79a4
Furry Trash <Fritz.gta.player@gmail.com>
2026-03-22 04:27:28 +0800

    Добавлена кнопка копирования почты
    favicon и шрифты перемещены в отдельные папки

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let showlinks = document.querySelector(".show-links");
 let infoblock = document.querySelector(".infoblock");
 let linkblock = document.querySelector(".linkblock");
 let backbutton = document.querySelector(".backbutton");
+let gmail = document.querySelector(".gmail");
+let copied = document.querySelector(".copied");
 
 showlinks.onclick = function () {
 	infoblock.classList.add("hidden");
@@ -15,4 +17,14 @@ backbutton.onclick = function() {
 	this.classList.add("hidden");
 	linkblock.classList.add("hidden");
 	showlinks.classList.remove("hidden");
+}
+
+gmail.onclick = function() {
+	navigator.clipboard.writeText("GrandTheftChatto@gmail.com");
+
+	copied.classList.add("copied-show");
+
+	setTimeout (() => {
+		copied.classList.remove("copied-show");
+	}, 1500);
 }
